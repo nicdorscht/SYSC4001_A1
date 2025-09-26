9ea55cf32d867257df1f46e5e40a0faf276c8e29
Picls2 <79704594+Picls2@users.noreply.github.com>
2025-09-26 09:44:46 -0400

    stuff

--- interrupts.cpp
+++ interrupts.cpp
@@ -5,30 +5,31 @@
  *
  */
 
-#include<interrupts.hpp>
+#include <interrupts.hpp>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
-    //vectors is a C++ std::vector of strings that contain the address of the ISR
-    //delays  is a C++ std::vector of ints that contain the delays of each device
-    //the index of these elemens is the device number, starting from 0
+    // vectors is a C++ std::vector of strings that contain the address of the ISR
+    // delays  is a C++ std::vector of ints that contain the delays of each device
+    // the index of these elemens is the device number, starting from 0
     auto [vectors, delays] = parse_args(argc, argv);
     std::ifstream input_file(argv[1]);
 
-    std::string trace;      //!< string to store single line of trace file
-    std::string execution;  //!< string to accumulate the execution output
+    std::string trace;     //!< string to store single line of trace file
+    std::string execution; //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-    //Store device table as a tuple
     int current_time = 0;
+    // Store device table as a tuple
     std::tuple<std::vector<std::string>, std::vector<int>> deviceTable = parse_args(argc, argv);
     int context_save_time = 10;
 
-
     /******************************************************************/
 
-    //parse each line of the input trace file
-    while(std::getline(input_file, trace)) {
+    // parse each line of the input trace file
+    while (std::getline(input_file, trace))
+    {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
@@ -39,7 +40,6 @@ int main(int argc, char** argv) {
         4. Switch to user mode, restore context
          */
         /************************************************************************/
-
     }
 
     input_file.close();
